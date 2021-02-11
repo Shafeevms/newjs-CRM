059af71911f9c68a2a94b8db484772b99166b840
Max Shafeev <shafeevms@gmail.com>
2021-02-11 15:49:17 +0300

    11.02

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import './grid.css'
-import './index.css'
+import './css/grid.css'
+import './css/index.css'
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
 
+tippy('.myButton', {
+  content: 'My tooltip!',
+});
+tippy('.myButton1', {
+  content: 'My tooltip1!',
+});
+
+
+// по ховеру изменить путь к картинке в кнопке "Изменить"
---
+++ src/templates/ClientLine.js
@@ -0,0 +1,40 @@
+export const ClientLine = () => (
+  `<li class="clients-item">
+    <ul class="client__list row no-gutters">
+      <li class="client__item col-md-1">
+        <h2 class="client__title client__title-id">123456</h2>
+      </li>
+      <li class="client__item col-md-3">
+        <h2 class="client__title">Скворцов Денис Юрьевич</h2>
+      </li>
+      <li class="client__item  col-md-2">
+        <h2 class="client__title">21.02.21</h2>
+        <span class="time">12:02</span>
+      </li>
+      <li class="client__item col-md-2">
+        <h2 class="client__title">21.02.21</h2>
+        <span class="time">12:02</span>
+      </li>
+      <li class="client__item-social col-md-2">
+        <ul class="social flex">
+          <li class="social__item myButton"><img src="./img/vk.svg" alt="vk"></li>
+          <li class="social__item myButton1"><img src="./img/fb.svg" alt="fb"></li>
+          <li class="social__item"><img src="./img/phone.svg" alt="phone"></li>
+          <li class="social__item"><img src="./img/mail.svg" alt="mail"></li>
+          <li class="social__item"><img src="./img/mail-2.svg" alt="mail"></li>
+          <li class="social__item"><img src="./img/vk.svg" alt="vk"></li>
+          <li class="social__item"><img src="./img/fb.svg" alt="fb"></li>
+          <li class="social__item"><img src="./img/phone.svg" alt="phone"></li>
+          <li class="social__item"><img src="./img/mail.svg" alt="mail"></li>
+          <li class="social__item"><img src="./img/mail-2.svg" alt="mail"></li>
+        </ul>
+      </li>
+      <li class="client__item col-md-1">
+        <button class="edit"><span class="btn-icon"><img src="./img/edit.svg"></span> Изменить</button>
+      </li>
+      <li class="client__item col-md-1">
+        <button class="cancel"><span class="btn-icon"><img src="./img/cancel.svg"></span>Удалить</button>
+      </li>
+    </ul>
+  </li>`
+)
