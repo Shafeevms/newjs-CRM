740d702e819e36f3e64cf9c6c28d8b3518bbae5c
Max Shafeev <shafeevms@gmail.com>
2021-02-15 15:00:38 +0300

    modal components started

---
+++ src/templates/AddSocialContacts.js
@@ -0,0 +1,26 @@
+export const ListOfSocialConstacts = () => (
+  `<ul class="modal__form-add-social add-social">
+  <!-- сюда вставляем ItemOfSocialContacts -->
+  </ul>`
+)
+
+export const ItemOfSocialContacts = ('rotate', 'd-none') => (
+  `<li class="add-social__item">
+    <img src="./img/social-arrow.png" class="option-img rotate">
+    <select name="social" class="add-social-select">
+      <option value="Телефон" class="add-social__option">Телефон</option>
+      <option value="Доп. телефон" class="add-social__option">Доп. телефон</option>
+      <option value="Email" class="add-social__option">Email</option>
+      <option value="Vk" class="add-social__option">Vk</option>
+      <option value="Facebook" class="add-social__option">Facebook</option>
+    </select>
+    <input type="text" class="add-social__input">
+    <button class="add-social__btn-clear d-none"><svg class="btn-clear-svg" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 0C2.682 0 0 2.682 0 6C0 9.318 2.682 12 6 12C9.318 12 12 9.318 12 6C12 2.682 9.318 0 6 0ZM6 10.8C3.354 10.8 1.2 8.646 1.2 6C1.2 3.354 3.354 1.2 6 1.2C8.646 1.2 10.8 3.354 10.8 6C10.8 8.646 8.646 10.8 6 10.8ZM8.154 3L6 5.154L3.846 3L3 3.846L5.154 6L3 8.154L3.846 9L6 6.846L8.154 9L9 8.154L6.846 6L9 3.846L8.154 3Z" fill="#B0B0B0"/>
+      </svg></button>
+  </li>`
+)
+
+// при нажатии на кнопку, добавляется ul, и нужно добавить padding-bottom 25px для add__contact -->
+// а также при следующих нажатиях на кнопку должно добавляться li -->
+// непонятно, как по-другому организовать верстку -->
