19be638704aaff337e5865b2e212cde1c0816582
Maxim <shafeevms@gmail.com>
2021-05-16 22:22:16 +0300

    tooltips changed

--- src/actions/tooltipsInit.js
+++ src/actions/tooltipsInit.js
@@ -1,11 +1,26 @@
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
+function prefix(type) {
+  let str = null;
+  if (type === 'tel') {
+    str = 'tel:';
+  } else if (type.includes('mail')) {
+    str = 'mailto:';
+  } else {
+    str = '';
+  }
+  return str;
+}
+
 export const tooltipsInit = ({ clients }) => {
   clients.forEach(client => {
     client?.contacts.forEach(contact => {
+      const str = prefix(contact.type);
       tippy(`.${contact.type}${client.id}`, {
-        content: `${contact.type}: ${contact.value}`,
+        content: `<a href="${str}${contact.value}" style="color:white">${contact.type}: ${contact.value}</a>`,
+        allowHTML: true,
+        interactive: true,
       });
     });
   });
